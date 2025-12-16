d3e7236e794f87f2d48353feb893c273ea5ddf8e
subodha-wijesekara <wijesekararsc@gmail.com>
2025-12-16 06:44:55 +0530

    image updation

--- src/components/HeroImageCarousel.tsx
+++ src/components/HeroImageCarousel.tsx
@@ -7,7 +7,7 @@ import styles from "./HeroImageCarousel.module.css";
 const IMAGES = [
     "/images/main1.jpg",
     "/images/main2.jpg",
-    "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?q=80&w=2064&auto=format&fit=crop",
+    "/images/main3.jpg",
     "https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?q=80&w=2070&auto=format&fit=crop"
 ];
 
